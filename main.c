74bb17abf656a13863470f27b1b8e46ae2ee125a
Gionas361 <giovannicarmona57@gmail.com>
2023-08-19 15:01:19 -0400

    works

--- main.c
+++ main.c
@@ -1,19 +1,40 @@
 #include "main.h"
 
 /**
-   main - runs the program lmao.
-   @argc: number of arguments.
-   @argv: array of arguments.
-   Return: status of the code
- */
+	* shell_loop_input - gets the input and runs it with the other functions.
+	*/
+void shell_loop_input(void)
+{
+	char *line;
+	char **args;
+	int status;
+
+	/** prints the $ and receives the input and runs command if any is provided */
+	do {
+		printf("$ ");
+		line = run_line_reader();
+		args = shell_line_splitter(line);
+		status = run_executer(args);
+
+		free(line);
+		free(args);
+	} while (status);
+}
+
+/**
+	* main - runs the program lmao.
+	* @argc: number of arguments.
+	* @argv: array of arguments.
+	* Return: status of the code
+	*/
 int main(int argc, char **argv)
 {
-  char **v = argv;
-  int c = argc;
-  (void)c;
-  (void)v;
+	char **v = argv;
+	int c = argc;
+	(void)c;
+	(void)v;
 
-  shell_loop_input();
-  
-  return (EXIT_SUCCESS);
+	shell_loop_input();
+
+	return (EXIT_SUCCESS);
 }
--- main.h
+++ main.h
@@ -14,12 +14,14 @@
 int cd_run(char **args);
 int help_run(char **args);
 int exit_run(char **args);
-int num_of_builtins();
+int aheago_run(char **args);
+int nekoarc_run(char **args);
+int num_of_builtins(void);
 int run_programm(char **args);
 int run_executer(char **args);
 char *run_line_reader();
 char **shell_line_splitter(char *line);
-void shell_loop_input();
+void shell_loop_input(void);
 int main(int argc, char **argv);
 
 #endif
